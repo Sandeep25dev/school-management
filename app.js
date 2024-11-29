08028891ca25be745c3feffa678b549e8ddb89ab
Sandeep25dev <sandeepkumar250903@gmail.com>
2024-11-29 09:52:05 +0000

    port changed

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const schoolRoutes = require("./routes/schoolRoutes");
 const sequelize = require("./configs/db");
 require("dotenv").config();
 
-const PORT = 3000;
-
 app.use(bodyParser.json());
 
 app.use("/api/schools", schoolRoutes);
 
-app.listen(PORT, async function () {
+app.listen(process.env.SERVER_PORT, async function () {
   try {
     await sequelize.sync({ force: false });
     console.log(`Server is running on PORT ${PORT}`);
