cdffa41e6d8a8d3f5f8929da299c556f5440a13f
Sandeep25dev <sandeepkumar250903@gmail.com>
2024-11-29 10:09:12 +0000

    port updated

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ app.use(bodyParser.json());
 
 app.use("/api/schools", schoolRoutes);
 
-app.listen(process.env.SERVER_PORT, async function () {
+const port = process.env.SERVER_PORT || 3001;
+
+app.listen(port, async function () {
   try {
     await sequelize.sync({ force: false });
-    console.log(`Server is live now`);
+    console.log(`Server is live on PORT ${port}`);
   } catch (err) {
     console.error("Error syncing database:", err);
   }
